16030b41467e5d90038468c40857ded90c7289fa
Keerthi <keerthi@southsoft.co.in>
2021-06-15 11:54:37 +0530

    newCommit

--- app/Http/Controllers/school/LeaveRequestController.php
+++ app/Http/Controllers/school/LeaveRequestController.php
@@ -19,9 +19,9 @@ class LeaveRequestController extends Controller
 {
     public function sendRequestStudent(request $request)
     {
-        $id=Auth::user()->id;
-        $Teacher=User::where('users.id',$id)
-        ->join('admission','users.parent','=','admission.parent')
+        $id=Auth::user()->admission_id;
+        $Teacher=User::where('users.admission_id',$id)
+        ->join('admission','users.admission_id','=','admission.admission_id')
         ->join('add_stream','admission.class','=','add_stream.id')
         ->select('add_stream.teacher')->first();
         $leaveRequest= LeaveRequest::create([
@@ -29,7 +29,7 @@ class LeaveRequestController extends Controller
             'from_date'=>$request->from_date,
             'to_date'=>$request->to_date,
             'reason'=>$request->reason,
-            'request_by'=>Auth::user()->id,
+            'request_by'=>Auth::user()->admission_id,
             'request_to'=>$Teacher->teacher,
             'date'=>date('Y-m-d')
         ]);
@@ -47,12 +47,13 @@ class LeaveRequestController extends Controller
     public function ShowLeaveRequestStudent(request $request)
     {
         $id=Auth::user()->id;
-
         $getRequest=LeaveRequest::where('request_by',$id)
-        ->select('date','from_date','to_date','reason',
+        ->join('users','leave_request.request_by','=','users.id')
+        ->join('admission','users.admission_id','=','admission.admission_id')
+        ->select('leave_request.date','from_date','to_date','leave_request.reason',
         db::raw('(CASE when accept = "542" then "Accepted"
                        when accept = "543" then "Rejected"
-                       else "pending"  end) as request'),'created_at','updated_at'
+                       else "pending"  end) as request'),'leave_request.created_at','leave_request.updated_at'
         )->get();
         if(!empty($getRequest)){
             return response()->json([
